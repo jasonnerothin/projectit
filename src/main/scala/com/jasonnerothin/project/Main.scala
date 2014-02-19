10027302eb82eea5fa02cccd46117f117ad209cb
Jason Nerothin <jason.nerothin@kohls.com>
2014-02-18 19:02:58 -0600

    Making implementation match blog post.

--- src/main/scala/com/jasonnerothin/project/Main.scala
+++ src/main/scala/com/jasonnerothin/project/Main.scala
@@ -12,11 +12,13 @@ object Main {
   def main(args: Array[String]){
 
     val projector = new Object with Projection
-    val inOneDimension = projector(List(Point(1,2,3),Point(2,3,4),Point(3,4,5)))
+    val inOneDimension = projector(List(Point(1,2,3),Point(1,3,4),Point(3,4,5)))
 
-    inOneDimension map{
-      case(p:Point) => println(p.asInt)
+    val seq = inOneDimension map{
+      case(p:Point) => p.asInt
     }
+
+    println(seq)
 
   }
 
